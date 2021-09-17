019137d41728699b700ea1336d062990737fbb8c
Hermanto <mamankuat@gmail.com>
2021-09-18 00:57:56 +0800

    part 4

---
+++ WorldComplete.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+export(String, FILE, "*.tscn") var next_world
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_WorldComplete_body_entered(body):
+	get_tree().change_scene(next_world)
+	print("level completed")
